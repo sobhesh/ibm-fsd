de3890eb841e6c26f0ef2a53db44f8af6feba994
Sobhesh Barun <sobheshbarun@gmail.com>
2019-07-12 14:11:15 +0530

    Update TimeTable.js

--- TimeTable.js
+++ TimeTable.js
@@ -12,4 +12,27 @@ function assign(){
     var Subject = document.getElementById("Subject").value;
     data=`{"time":"${time}","Subject":"${Subject}"}`;
     localStorage.setItem(`${time}`,`${data}`);
+    var id=document.getElementById("manualID").value;
+    Object.keys(localStorage).forEach((e)=>{
+        let v=JSON.parse(localStorage.getItem(e))
+        if(v.ID=="1"){
+            document.getElementById("s1").innerHTML=v.name;
+            document.getElementById("s1").innerHTML=v.name;
+        }
+        if(v.ID=="2"){
+            document.getElementById("s2").innerHTML=v.name;
+        }
+        if(v.ID=="3"){
+            document.getElementById("s3").innerHTML=v.name;
+        }
+        if(v.ID=="4"){
+            document.getElementById("s4").innerHTML=v.name;
+        }
+        if(v.ID=="5"){
+            document.getElementById("s5").innerHTML=v.name;
+        } 
+        if(v.ID=="6"){
+            document.getElementById("s6").innerHTML=v.name;
+        }
+    })
 }
